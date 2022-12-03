f575d2bd6bfd1cf09aa126c8de2a2f7f0374037a
ilfa <ilfahassan@gmail.com>
2022-12-03 13:38:28 +0000

    fiwed isManchester and comments

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,31 +1,26 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   return sandwich.fillings;
-  // Your code here!
+ 
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  let isManchester= false;
-  if(person.city==='Manchester')
-  {
-    isManchester=true;
+    return (person.city==='Manchester')
+  
   }
-  return isManchester;
-  // Your code here!
-}
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   let result=Math.ceil(people/40);
   return result;
-  // Your code here!
+ 
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   return arr.filter(x => x=='sheep').length;
-  // Your code here!
+  
 }
 
 export function hasMPostCode(person) {
@@ -36,5 +31,5 @@ export function hasMPostCode(person) {
     hasPostCode=true;
   }
   return hasPostCode;
-  // Your code here!
+ 
 }
