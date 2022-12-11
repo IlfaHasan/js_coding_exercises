e7a2d4a0da7481d9de0930987be571c8b02e3f3d
ilfa <ilfahassan@gmail.com>
2022-12-11 23:37:48 +0000

    Corrected getScreenTime function

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -37,13 +37,76 @@ import {
   });
 
   describe("getScreentimeAlertList", () => {
-    test("returns the user names of users with screentime greater than 100", () => {
-        const users=[{username: "beth_1234",
-        name: "Beth Smith",
-        screenTime: [{ date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40} },
-                     { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31} },
-                     { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19} },]}]
-      expect(getScreentimeAlertList(users,"2019-05-01")).toEqual(["beth_1234"]);
-           
-    });  
-  });
+    test('it throws an error if not passed users or date ', () => {
+        const users = [
+            {
+                username: "beth_1234",
+                name: "Beth Smith",
+                screenTime: [
+                    { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40 } },
+                    { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31 } },
+                    { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19 } },
+                    { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61 } },
+                ]
+            },
+        ];
+        expect(() => {
+            getScreentimeAlertList();
+        }).toThrow("users is required");
+        expect(() => {
+            getScreentimeAlertList(users);
+        }).toThrow("date is required");
+    });
+
+    test('return empty array if date not found in user array  or no user exceed limit usage time in this date', () => {
+        const users = [
+            {
+                username: "beth_1234",
+                name: "Beth Smith",
+                screenTime: [
+                    { date: "2019-05-01", usage: { twitter: 34, instagram: 22, facebook: 40 } },
+                    { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31 } },
+                    { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19 } },
+                    { date: "2019-06-04", usage: { twitter: 10, instagram: 20, facebook: 70 } },
+                ]
+            },
+            {
+                username: "Ilfa_1234",
+                name: "Ilfa",
+                screenTime: [
+                    { date: "2019-06-01", usage: { twitter: 34, instagram: 8, facebook: 40 } },
+                    { date: "2019-06-02", usage: { twitter: 56, instagram: 40, facebook: 31 } },
+                    { date: "2019-06-03", usage: { twitter: 12, instagram: 0, facebook: 19 } },
+                    { date: "2019-06-04", usage: { twitter: 10, instagram: 40, facebook: 10 } },
+                ]
+            }
+        ];
+        expect(getScreentimeAlertList(users, '2019-06-07')).toStrictEqual([]);
+        expect(getScreentimeAlertList(users, '2019-06-04')).toStrictEqual([]);
+    });
+    test('return all users in array if usage record are above 100 min in given date', () => {
+        const users = [
+            {
+                username: "beth_1234",
+                name: "Beth Smith",
+                screenTime: [
+                    { date: "2019-06-01", usage: { twitter: 34, instagram: 22, facebook: 60 } },
+                    { date: "2019-05-02", usage: { twitter: 56, instagram: 40, facebook: 31 } },
+                    { date: "2019-05-03", usage: { twitter: 12, instagram: 15, facebook: 19 } },
+                    { date: "2019-05-04", usage: { twitter: 10, instagram: 56, facebook: 61 } },
+                ]
+            },
+            {
+                username: "Ilfa_1234",
+                name: "Ilfa ",
+                screenTime: [
+                    { date: "2019-06-01", usage: { twitter: 34, instagram: 60, facebook: 40 } },
+                    { date: "2019-06-02", usage: { twitter: 56, instagram: 40, facebook: 31 } },
+                    { date: "2019-06-03", usage: { twitter: 12, instagram: 0, facebook: 19 } },
+                    { date: "2019-06-04", usage: { twitter: 10, instagram: 56, facebook: 61 } },
+                ]
+            }
+        ];
+        expect(getScreentimeAlertList(users, '2019-06-01')).toStrictEqual(["beth_1234", "Ilfa_1234"]);
+    });
+});
